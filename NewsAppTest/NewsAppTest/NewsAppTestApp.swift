8107a37b55f6342b6dca9559649f908f89a3a23a
PunitNaik <punit.ramchandra@byjus.com>
2024-05-20 14:56:23 +0530

    deleted default files from the project

--- NewsAppTest/NewsAppTest/NewsAppTestApp.swift
+++ NewsAppTest/NewsAppTest/NewsAppTestApp.swift
@@ -6,31 +6,15 @@
 //
 
 import SwiftUI
-import SwiftData
 import HomeModule
 
 @main
 struct NewsAppTestApp: App {
     private let homeRouter = HomeRouter()
 
-    var sharedModelContainer: ModelContainer = {
-        let schema = Schema([
-            Item.self,
-        ])
-        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
-
-        do {
-            return try ModelContainer(for: schema, configurations: [modelConfiguration])
-        } catch {
-            fatalError("Could not create ModelContainer: \(error)")
-        }
-    }()
-
     var body: some Scene {
         WindowGroup {
             homeRouter.buildDashboardView()
-//            DashboardView(viewModel: DashboardViewVM(homeService: nil))
         }
-        .modelContainer(sharedModelContainer)
     }
 }
